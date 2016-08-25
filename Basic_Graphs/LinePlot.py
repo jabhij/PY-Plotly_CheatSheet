c589419e44305c5cef9a333ec5f9630163fdba3e
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-08-25 21:57:33 +0530

    LinePlot

--- Basic_Graphs/LinePlot.py
+++ Basic_Graphs/LinePlot.py
@@ -1,1 +1,10 @@
-Loading code, please wait...
+# Importing Plotly Module
+import plotly as py
+import plotly.graph_objs as go
+
+# Traces for Line Plot
+trace1 = go.scatter(
+  x=[1,5], y=[1,5])
+trace2 = go.scatter(
+  x=[1,5], y=[5,1])
+py.iplot([trace1, trace2])
