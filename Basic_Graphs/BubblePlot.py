95679df88285782d94a763a997199a6138ec35ef
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-09-16 12:05:09 +0530

    BubblePlot

---
+++ Basic_Graphs/BubblePlot.py
@@ -0,0 +1,11 @@
+# Importing Plotly Module
+import plotly as py
+import plotly.graph_objs as go
+
+trace = go.Scatter(
+  x = [1,2,3], y = [1,2,3],
+  marker = dict(
+  color = ['red','green']
+  size = [25, 50, 200]),
+  mode = 'markers')
+py.iplot([trace])
