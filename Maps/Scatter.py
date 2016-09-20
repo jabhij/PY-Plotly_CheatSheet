3baf91f067e60ec2c9df6c6c30c3952aae064eaf
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-09-20 18:53:46 +0530

    Scatter Map

---
+++ Maps/Scatter.py
@@ -0,0 +1,13 @@
+
+# Using dict elements
+trace = dict(
+  type = 'Scattergo'
+  
+  # Longitude and Latitude
+  lon = [28.4355645, 26.3556464], lat = [45.3350533, 30.4245355],
+
+  # Plot
+  marker = ['LocA', 'LocB'],
+  mode = 'markers')
+  
+py.iplot([trace])
